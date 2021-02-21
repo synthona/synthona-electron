9018aaf8c2be0ba79aa018106037c69cc7003b34
geographicscard <63616362+synthona@users.noreply.github.com>
2021-02-20 18:49:11 -0600

    changed default build settings for mac to dmg instead of zip

--- forge.config.js
+++ forge.config.js
@@ -24,7 +24,7 @@ module.exports = {
       },
     },
     {
-      name: '@electron-forge/maker-zip',
+      name: '@electron-forge/maker-dmg',
       platforms: ['darwin'],
       config: {
         'name': 'synthona',

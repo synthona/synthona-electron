17f0aee524f955cac6d17bdcdb340fa3ec9246b0
geographicscard <63616362+synthona@users.noreply.github.com>
2021-02-20 18:32:56 -0600

    tweak to how creating new windows works on mac

--- app.js
+++ app.js
@@ -337,7 +337,7 @@ app.on('before-quit', () => {
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (/*BrowserWindow.getAllWindows().length === 0 && */ serverReady && electronReady) {
+  if (BrowserWindow.getAllWindows().length === 0 && serverReady && electronReady) {
     mainWindow();
   }
 });

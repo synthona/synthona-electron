60fe1902c5ada2bec066e7e6cc11b2b14fcead9a
geographicscard <63616362+synthona@users.noreply.github.com>
2021-03-02 18:51:56 -0600

    updated client build

---
+++ src/client/precache-manifest.5da9ab8a8c863ea6e3d3e15cdc2fe3e0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beab65abaa4efc7a5b4a04e1aa88f32f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e16da49fd0bd80e90877",
+    "url": "/static/css/2.96c4aec3.chunk.css"
+  },
+  {
+    "revision": "920ed7d6f06bbd446fe6",
+    "url": "/static/css/main.264ef1c6.chunk.css"
+  },
+  {
+    "revision": "e16da49fd0bd80e90877",
+    "url": "/static/js/2.8e01f747.chunk.js"
+  },
+  {
+    "revision": "39e8de98b08f70f4d64d7c891c5fabba",
+    "url": "/static/js/2.8e01f747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920ed7d6f06bbd446fe6",
+    "url": "/static/js/main.291974eb.chunk.js"
+  },
+  {
+    "revision": "ac8a3bfceaf4401a606c",
+    "url": "/static/js/runtime-main.08bbf159.js"
+  },
+  {
+    "revision": "22dc8cce953117922508a255ba5638ce",
+    "url": "/static/media/cloud9.22dc8cce.png"
+  },
+  {
+    "revision": "f2efb2204eecaff0425b51611841642e",
+    "url": "/static/media/help.f2efb220.png"
+  },
+  {
+    "revision": "b740604b000e994da920f7ae8c8ac436",
+    "url": "/static/media/synthona-logo.b740604b.png"
+  }
+]);
--- src/client/service-worker.js
+++ src/client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.138bbd77965ca10a1baf490d5a8c10e9.js"
+  "/precache-manifest.5da9ab8a8c863ea6e3d3e15cdc2fe3e0.js"
 );
 
 self.addEventListener('message', (event) => {

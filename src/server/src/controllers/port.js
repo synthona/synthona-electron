da405fb7c98ccdd3926c3e6e4b5fe2b834e309b5
geographicscard <63616362+synthona@users.noreply.github.com>
2021-07-03 22:25:09 -0500

    we should also add the preview image for user nodes

--- src/server/src/controllers/port.js
+++ src/server/src/controllers/port.js
@@ -680,7 +680,7 @@ exports.unpackSynthonaImport = async (req, res, next) => {
 							throw err;
 						}
 						const dbFilePath = path.join(filePath, fileEntry.name);
-						const previewPath = nodeImport.type === 'image' ? dbFilePath : null;
+						const previewPath = nodeImport.type === 'image' || 'user' ? dbFilePath : null;
 						// generate node
 						newNode = await node.create(
 							{

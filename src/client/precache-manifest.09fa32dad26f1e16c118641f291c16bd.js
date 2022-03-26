6296158f84a7b19e1bc7e4089c0f09fe11bb36d1
geographicscard <63616362+synthona@users.noreply.github.com>
2022-03-25 20:10:44 -0500

    updated the client build

---
+++ src/client/precache-manifest.09fa32dad26f1e16c118641f291c16bd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eacdd08516ccd44cb8b602c31f112ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0b81419456488dc795a",
+    "url": "/static/css/2.c2cd6797.chunk.css"
+  },
+  {
+    "revision": "87a18c3cd9fc858224c3",
+    "url": "/static/css/main.7e8e3b09.chunk.css"
+  },
+  {
+    "revision": "f0b81419456488dc795a",
+    "url": "/static/js/2.9a59d5ca.chunk.js"
+  },
+  {
+    "revision": "39e8de98b08f70f4d64d7c891c5fabba",
+    "url": "/static/js/2.9a59d5ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87a18c3cd9fc858224c3",
+    "url": "/static/js/main.18a72321.chunk.js"
+  },
+  {
+    "revision": "ac8a3bfceaf4401a606c",
+    "url": "/static/js/runtime-main.08bbf159.js"
+  },
+  {
+    "revision": "22dc8cce953117922508a255ba5638ce",
+    "url": "/static/media/cloud9.22dc8cce.png"
+  },
+  {
+    "revision": "20cda3d181641cdddd9734491f33bcdf",
+    "url": "/static/media/missing-file.20cda3d1.png"
+  },
+  {
+    "revision": "b740604b000e994da920f7ae8c8ac436",
+    "url": "/static/media/synthona-logo.b740604b.png"
+  }
+]);
